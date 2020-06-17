f81473c495dd17f73c1c0da9174b003c1ecc8b84
Ruslan <ruslan.borsov@mail.ru>
2020-06-17 21:39:43 +0300

    add class Singleton

--- patterns/Singleton.php
+++ patterns/Singleton.php
@@ -1,15 +1,51 @@
 <?php
 
-class Singlton {
+/**
+ * Класс Одиночка(Singleton) порождающий паттерн,
+ * который гарантирует, что для определенного класса
+ * будет создан только один объект
+ */
 
-    private $instance = null;
+class Singleton {
 
-    private function __construct()
+    private static $instance = null;
+    /*
+     * При вызове метода, возвращает единственный экземпляр класса
+     * из $instance (создается при первом вызове)     *
+     */
+    public static function getInstance() {
+        if (static::$instance === null) {
+            static::$instance = new Singleton();
+        }
+
+        return static::$instance;
+    }
+
+    // не разрешает создавать объект вне класса
+    protected function __construct() { }
+    // не разрешает клонировать объект
+    protected function __clone() { }
+    // запрещает восстановить объект
+    public function __wakeup()
     {
-
+        throw new \Exception("Cannot unserialize a singleton.");
     }
 
-    private getInstance() {
-        if ($this)
+    /*
+     * остальная бизнес логики класса
+     */
+    public function foo() {
+        return 'foo';
     }
+
+}
+
+// проверка
+$s1 = Singleton::getInstance();
+$s2 = Singleton::getInstance();
+
+if ($s1 === $s2) {
+    echo "Одиночка(Singleton) работает, в переменных один и тот же экземпляр";
+} else {
+    echo "Одиночка(Singleton) класс с ошибкой, в переменных разные экземпляры";
 }
